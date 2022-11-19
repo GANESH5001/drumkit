d82fa5633cb84a67bd76bc99f5a0d0f695c7d2d9
GANESH5001 <118376542+GANESH5001@users.noreply.github.com>
2022-11-19 19:22:37 +0530

    Update drum.js

--- drum.js
+++ drum.js
@@ -13,37 +13,37 @@ function perform()
    switch (efg)
     {
     case "w":
-     var ab=new Audio("sounds/tom-1.mp3");
+     var ab=new Audio("tom-1.mp3");
      ab.play();
         break;
     
     case "a":
-            var abc=new Audio("sounds/tom-2.mp3");
+            var abc=new Audio("tom-2.mp3");
             abc.play();
                break;
 
      case "s":
-     var abcd=new Audio("sounds/tom-3.mp3");
+     var abcd=new Audio("tom-3.mp3");
      abcd.play();
         break;
 
     case "d":
-     var abcde=new Audio("sounds/tom-4.mp3");
+     var abcde=new Audio("tom-4.mp3");
      abcde.play();
         break;
 
     case "j":
-     var abs=new Audio("sounds/snare.mp3");
+     var abs=new Audio("snare.mp3");
      abs.play();
         break;
 
     case "k":
-     var absw=new Audio("sounds/crash.mp3");
+     var absw=new Audio("crash.mp3");
      absw.play();
         break;
 
     case "l":
-     var abfr=new Audio("sounds/kick-bass.mp3");
+     var abfr=new Audio("kick-bass.mp3");
      abfr.play();
         break;
     default:alert("SORRY FOR INCONVENIENCE");
